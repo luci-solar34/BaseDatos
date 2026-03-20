637f3172f758f9b41e5311d1aaea7c2e450f1d0d
andreatrujillo1-tech <andreatrujillo1@upb.edu>
2026-03-20 19:11:53 -0400

    Cambios en el explorador del los tags

--- MVC/views/tags.php
+++ MVC/views/tags.php
@@ -1,37 +1,39 @@
-<h1>Explorar Tags</h1>
+<h1 class="page-title">Explorar Tags</h1>
+<link rel="stylesheet" href="/LASK/public/css/explorar-tags.css">
+<link href="https://fonts.googleapis.com/css2?family=VT323&display=swap" rel="stylesheet">
 
 <?php if($canCreateTag): ?>
-    <p>
+    <p class="create-tag">
         <a href="<?= BASE_URL ?>/tag/create">Crear nuevo tag</a>
     </p>
 <?php endif; ?>
 
 <?php if($flashMessage): ?>
-    <div style="padding:10px; margin:10px 0; border:1px solid green; background:#e6ffe6;">
+    <div class="flash-message">
         <?= htmlspecialchars($flashMessage) ?>
     </div>
 <?php endif; ?>
 
-<form action="<?= htmlspecialchars(BASE_URL . '/tags') ?>" method="GET">
+<form action="<?= htmlspecialchars(BASE_URL . '/tags') ?>" method="GET" class="search-form">
     <input type="text" name="q" value="<?= htmlspecialchars($query ?? '') ?>" placeholder="Buscar tags">
     <button type="submit">Buscar</button>
 </form>
 
 <?php if(empty($tags)): ?>
-    <p>No se encontraron tags con esa búsqueda.</p>
+    <p class="no-tags">No se encontraron tags con esa búsqueda.</p>
 <?php else: ?>
-    <ul>
+    <ul class="tag-list">
         <?php foreach($tags as $tag): ?>
-            <li>
-                <a href="<?= BASE_URL ?>/tag?id=<?= (int)$tag['id_tag'] ?>">
+            <li class="tag-item">
+                <a href="<?= BASE_URL ?>/tag?id=<?= (int)$tag['id_tag'] ?>" class="tag-link">
                     <?= htmlspecialchars($tag['nombre_tag']) ?>
                 </a>
                 <?php if(!empty($tag['descripcion_tag'])): ?>
-                    <div><?= htmlspecialchars($tag['descripcion_tag']) ?></div>
+                    <div class="tag-desc"><?= htmlspecialchars($tag['descripcion_tag']) ?></div>
                 <?php endif; ?>
             </li>
         <?php endforeach; ?>
     </ul>
 <?php endif; ?>
 
-<a href="<?= htmlspecialchars(BASE_URL) ?>">Volver al home</a>
+<a href="<?= htmlspecialchars(BASE_URL) ?>" class="back-home">Volver al home</a>
